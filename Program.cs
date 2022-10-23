23af711379c31506e5b82cf2bc4c67016d02d4dd
hatfieldhm1 <113749316+hatfieldhm1@users.noreply.github.com>
2022-10-23 18:56:25 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+double calculateRetail();
+{
+    double Cost,
+           Markup;
+
+    do
+    {
+        cout << "What is the item's wholesale cost? ";
+        cin >> Cost;
+        if (Cost < 0)
+        {
+            cout << "Error!\n"
+         << "Wholesale cost must be a positive number.\n";
+        }
+    } while (!(Cost > 0));
+    do
+    {
+        cout << "What is the item's markup percentage? ";
+        cin >> Markup;
+        if (Markup < 0)
+        {
+            cout << "Error!\n"
+         << "The markup percentage must be a positive number.\n";
+        }
+    } while (!(Markup > 0));
+
+    Markup /= 100;
+    return Cost * (1 + Markup);
+}
