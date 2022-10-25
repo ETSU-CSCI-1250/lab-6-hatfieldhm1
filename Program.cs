0d995358a902e6aa97383eab246c8ab238522b96
hatfieldhm1 <113749316+hatfieldhm1@users.noreply.github.com>
2022-10-24 21:55:57 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,32 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 Console.WriteLine("Hello, World!");
 
-double calculateRetail();
+public static void main(String[] args)
 {
-    double Cost,
-           Markup;
+ 
 
-    do
+    isPrime(27);
+}
+public static boolean isPrime(int number)
+{
+    for (int i = 2; i < number; i++)
     {
-        cout << "What is the item's wholesale cost? ";
-        cin >> Cost;
-        if (Cost < 0)
+        if (number % i == 0)
         {
-            cout << "Error!\n"
-         << "Wholesale cost must be a positive number.\n";
+            return false;
         }
-    } while (!(Cost > 0));
-    do
-    {
-        cout << "What is the item's markup percentage? ";
-        cin >> Markup;
-        if (Markup < 0)
-        {
-            cout << "Error!\n"
-         << "The markup percentage must be a positive number.\n";
-        }
-    } while (!(Markup > 0));
+    }
 
-    Markup /= 100;
-    return Cost * (1 + Markup);
+    return true;
 }
