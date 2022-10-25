21bb13559ee87526967b9dfb37396a354c141a2b
hatfieldhm1 <113749316+hatfieldhm1@users.noreply.github.com>
2022-10-24 22:09:12 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, World!");
+
+static bool IsPrime(int number)
+{
+    if (number % 2 == 0 && number !=2)
+        return false;
+}
+
+{
+    if (number % 3 == 0)
+        return false;
+}
+
+{
+    if (number % 5 == 0)
+        return false;
+}
+
+{
+    if (number % 7 == 0)
+        return false;
+}
+
+{
+    if (number % 11 == 0)
+        return false;
+}
+
+{
+    if (number % 13 == 0)
+        return false;
+}
